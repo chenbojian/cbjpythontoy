749cc8d7e374369f2dffc1daa1bcb7e7c5c51fdd
chenbojian <chenbojian@foxmail.com>
2015-05-16 22:36:04 +0800

    add learnpytest

---
+++ learnpytest/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+def pytest_addoption(parser):
+    parser.addoption("--cmdopt", action="store", default="type1",
+            help="my options: type1 or type2")
+@pytest.fixture
+def cmdopt(request):
+    return request.config.getoption("--cmdopt")
