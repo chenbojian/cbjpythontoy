56fec3dda702965520d2f3ee9e283e4aae27d297
unknown <chenbojian@foxmail.com>
2016-02-25 00:07:20 +0800

    refactor and update

--- zipfile/src/unzipcomic.py
+++ zipfile/src/unzipcomic.py
@@ -1,17 +1,14 @@
+import shutil
 import subprocess as sp
 from pathlib import Path
+import argparse
 
 
-def first_exist_path(paths):
-    for path in paths:
-        if Path(path).exists():
-            return path
-    raise Exception()
-
-
-def find_seven_zip():
-    paths = [r'C:\7z.exe', r'C:\Program Files\7-Zip\7z.exe']
-    return first_exist_path(paths)
+def first(action, iterable):
+    for i in iterable:
+        if action(i):
+            return i
+    return None
 
 
 def generate_args(**kwargs):
@@ -26,18 +23,46 @@ def generate_args(**kwargs):
 
 class SevenZip(object):
     def __init__(self):
-        self.exe = find_seven_zip()
+        dirs = [r'C:\7z.exe', r'C:\Program Files\7-Zip\7z.exe']
+        self.exe = first(lambda d: Path(d).exists(), dirs)
+        self.output = []
+        self.err = []
 
     def list_zipped_file(self, file, password=None):
         args = generate_args(exe=self.exe, password=password, file=file, action='l')
-        process = sp.Popen(args, stdout=sp.PIPE)
-        output = process.stdout.read()
-        process.wait()
-        return output.decode('gbk')
+
+        return self.__execute(args)
 
     def unzip_zipped_file(self, file, password=None, destination='tmp'):
-        args = generate_args(exe=self.exe, password=password, file=file, action='x', destination=destination)
-        process = sp.Popen(args, stdout=sp.PIPE)
-        output = process.stdout.read()
+        if password is None:
+            args = generate_args(exe=self.exe, file=file, action='x', destination=destination)
+        else:
+            args = generate_args(exe=self.exe, password=password, file=file, action='x', destination=destination)
+
+        return self.__execute(args)
+
+    def __execute(self, args):
+        process = sp.Popen(args, stdin=sp.DEVNULL, stdout=sp.PIPE, stderr=sp.PIPE)
+        self.output.append(process.stdout.read())
+        self.err.append(process.stderr.read())
         process.wait()
-        return output
+        return process.returncode
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', dest='password', default=None)
+    parser.add_argument('-o', dest='out_dir', default='tmp')
+    input_args = parser.parse_args()
+
+    if Path(input_args.out_dir).exists():
+        shutil.rmtree(input_args.out_dir)
+
+    sz = SevenZip()
+    paths = Path('.').glob('*.rar')
+    for p in paths:
+        rt = sz.unzip_zipped_file(p.name, password=input_args.password, destination=input_args.out_dir)
+        if rt == 0:
+            print(p.name, 'Ok')
+        else:
+            print(p.name, 'Fail')
--- zipfile/test/test.py
+++ zipfile/test/test.py
@@ -5,18 +5,20 @@ from unzipcomic import *
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.sz = SevenZip()
+
     def test_find_seven_zip(self):
-        self.assertEqual(find_seven_zip(), r'C:\Program Files\7-Zip\7z.exe')
+        self.assertEqual(self.sz.exe, r'C:\Program Files\7-Zip\7z.exe')
 
     def test_list_file_with_seven_zip(self):
-        sz = SevenZip()
-        output = sz.list_zipped_file('1.rar', '扶她奶茶')
-        self.assertTrue(len(output) > 0)
+        return_code = self.sz.list_zipped_file('1.rar', '扶她奶茶')
+        self.assertEqual(return_code, 0)
 
     def test_unzip_file_with_seven_zip(self):
         self.assertFalse(Path('tmp').exists())
-        sz = SevenZip()
-        output = sz.unzip_zipped_file('1.rar', '扶她奶茶', 'tmp')
-        self.assertTrue(len(output) > 0)
+        return_code = self.sz.unzip_zipped_file('1.rar', '扶她奶茶', 'tmp')
+        self.assertEqual(return_code, 0)
         self.assertTrue(Path('tmp').exists())
         shutil.rmtree('tmp')
